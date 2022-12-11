506bb22f901e319f14680ce139df7711a051fbba
hd <hannes@unite.co.at>
2022-12-11 23:18:29 +0100

    fix diktator due to previous model changes

--- bin/diktator/main.rs
+++ bin/diktator/main.rs
@@ -16,7 +16,11 @@ pub fn handle_user_command(user: UserCommand) {
     match command {
         UserSubcommand::Create { name: n, email: e } => {
             let new_user = NewUser { name: n, email: e };
-            User::create(new_user);
+            let user = User::create(new_user);
+            match user {
+                Ok(_) => println!("Created User"),
+                Err(_) => println!("Could not create User"),
+            };
         }
         UserSubcommand::Update {
             id: i,
@@ -28,13 +32,30 @@ pub fn handle_user_command(user: UserCommand) {
                 name: n,
                 email: e,
             };
-            User::update(update_user);
+            let user = User::update(update_user);
+            match user {
+                Ok(_) => println!("Updated User"),
+                Err(_) => println!("Could not update User"),
+            };
         }
         UserSubcommand::Delete { id: i } => {
-            User::delete(i);
+            let user = User::delete(i);
+            match user {
+                Ok(_) => println!("Deleted User"),
+                Err(_) => println!("Could not delete User"),
+            };
         }
         UserSubcommand::Show => {
-            User::get_all();
+            
+            let user = User::get_all();
+            match user {
+                Ok(u) => {
+                    for user in u {
+                        println!("{user:?}");
+                    }
+                }
+                Err(_) => println!("Could not query User"),
+            };
         }
     }
 }
