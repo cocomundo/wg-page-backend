fcc10811b4f1ec3b85794095f027d47943b53eb0
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-12-12 23:07:43 +0100

    One does not simply
    touch RUST_LOG and RUST_BACKTRACE from within a binary.

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ use wg_page_backend::api::{
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "debug");
-    std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
     HttpServer::new(move || {
